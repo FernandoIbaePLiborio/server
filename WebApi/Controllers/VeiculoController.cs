91dd88e212152734ae07e6bd9f0f72041b3d9f10
fernando.liborio@opet.edu.br <fernando.liborio@opet.edu.br>
2018-08-17 03:12:13 -0300

    version 1.2

--- WebApi/Controllers/VeiculoController.cs
+++ WebApi/Controllers/VeiculoController.cs
@@ -22,9 +22,9 @@ namespace WebApi.Controllers {
         [AcceptVerbs("PUT")]
         [Route("AlterarVeiculo")]
         public string SalvarVeiculo(Veiculo veiculo) {
-            listaVeiculos.Where(v => v.Codigo == veiculo.Codigo).Select(s => {
-                s.Codigo = veiculo.Codigo;
-                s.Fabricante = veiculo.Fabricante;
+            listaVeiculos.Where(v => v.codigo == veiculo.codigo).Select(s => {
+                s.codigo = veiculo.codigo;
+                s.fabricante = veiculo.fabricante;
                 return s;
             }).ToList();
             return "Veículo alterado com sucesso!";
@@ -33,7 +33,7 @@ namespace WebApi.Controllers {
         [AcceptVerbs("DELETE")]
         [Route("ExcluirVeiculo/{codigo}")]
         public string ExcluirVeiculo(int codigo) {
-            Veiculo veiculo = listaVeiculos.Where(v => v.Codigo == codigo)
+            Veiculo veiculo = listaVeiculos.Where(v => v.codigo == codigo)
                 .Select(v => v)
                 .First();
             listaVeiculos.Remove(veiculo);
@@ -44,23 +44,32 @@ namespace WebApi.Controllers {
         [Route("ConsultarVeiculos")]
         public List<Veiculo> ListarVeiculos() {
             listaVeiculos = new List<Veiculo>()
-            { new Veiculo() { Codigo = 1, Placa = "BMW-2558", Frota = "CCV", Propriedade = true , TipoVeiculo = "Automóvel", TipoCarroceria = "Pick-Up",
-                Fabricante = "Fiat", Modelo = "Uno", AnoFabricacao = 2016, AnoModelo = 2017, Pais = "Brasil", Cidade= "Curitiba", Tara = 8500, CapacidadeDeCarga = 15000, Altura = 165,
-            Largura = 154, Comprimento = 300, Motorista = "Katia", Proprietario = "Lineu" },
-            new Veiculo() { Codigo = 2, Placa = "BMW-2558", Frota = "CCV", Propriedade = true , TipoVeiculo = "Automóvel", TipoCarroceria = "Pick-Up",
-                Fabricante = "Fiat", Modelo = "Uno", AnoFabricacao = 2016, AnoModelo = 2017, Pais = "Brasil", Cidade= "Curitiba",Tara = 8500, CapacidadeDeCarga = 15000, Altura = 165,
-            Largura = 154, Comprimento = 300, Motorista = "Katia", Proprietario = "Lineu" },
-            new Veiculo() { Codigo = 3, Placa = "BMW-2558", Frota = "CCV", Propriedade = true , TipoVeiculo = "Automóvel", TipoCarroceria = "Pick-Up",
-                Fabricante = "Fiat", Modelo = "Uno", AnoFabricacao = 2016, AnoModelo = 2017, Pais = "Brasil", Cidade= "Curitiba", Tara = 8500, CapacidadeDeCarga = 15000, Altura = 165,
-            Largura = 154, Comprimento = 300, Motorista = "Katia", Proprietario = "Lineu" },
-            new Veiculo() { Codigo = 4, Placa = "BMW-2558", Frota = "CCV", Propriedade = true , TipoVeiculo = "Automóvel", TipoCarroceria = "Pick-Up",
-                Fabricante = "Fiat", Modelo = "Uno", AnoFabricacao = 2016, AnoModelo = 2017, Pais = "Brasil", Cidade= "Curitiba", Tara = 8500, CapacidadeDeCarga = 15000, Altura = 165,
-            Largura = 154, Comprimento = 300, Motorista = "Katia", Proprietario = "Lineu" },
-            new Veiculo() { Codigo = 4, Placa = "BMW-2558", Frota = "CCV", Propriedade = true , TipoVeiculo = "Automóvel", TipoCarroceria = "Pick-Up",
-                Fabricante = "Fiat", Modelo = "Uno", AnoFabricacao = 2016, AnoModelo = 2017, Pais = "Brasil", Cidade= "Curitiba", Tara = 8500, CapacidadeDeCarga = 15000, Altura = 165,
-            Largura = 154, Comprimento = 300, Motorista = "Katia", Proprietario = "Lineu" }
+            { new Veiculo() { codigo = 1, placa = "BMW-2558", frota = "CCV", propriedade = true , tipoVeiculo = "Automóvel", tipoCarroceria = "Pick-Up",
+                fabricante = "Fiat", modelo = "Uno", anoFabricacao = 2016, anoModelo = 2017, pais = "Brasil", cidade= "Curitiba", tara = 15000, capacidadeCarga = 15000, altura = 165,
+            largura = 154, comprimento = 300, motorista = "Carolina Benato", proprietario = "Denise Fraga" },
+            new Veiculo() { codigo = 2, placa = "SDF-3215", frota = "POLICIAL", propriedade = true , tipoVeiculo = "Automóvel", tipoCarroceria = "Sedan",
+                fabricante = "Fiat", modelo = "Uno", anoFabricacao = 2016, anoModelo = 2017, pais = "Brasil", cidade= "Curitiba", tara = 500, capacidadeCarga = 15000, altura = 165,
+            largura = 154, comprimento = 300, motorista = "Katia Holtz", proprietario = "Lineu Pereira" },
+            new Veiculo() { codigo = 3, placa = "BSE-2558", frota = "CCV", propriedade = true , tipoVeiculo = "Automóvel", tipoCarroceria = "Perua",
+                fabricante = "Fiat", modelo = "Uno", anoFabricacao = 2016, anoModelo = 2017, pais = "Brasil", cidade= "Curitiba", tara = 8500, capacidadeCarga = 15000, altura = 165,
+            largura = 154, comprimento = 300, motorista = "Daniel Plitz", proprietario = "Amanda Virginea" },
+            new Veiculo() { codigo = 4, placa = "ASD-3223", frota = "Treinamento", propriedade = true , tipoVeiculo = "Automóvel", tipoCarroceria = "Jipe",
+                fabricante = "Fiat", modelo = "Uno", anoFabricacao = 2016, anoModelo = 2017, pais = "Brasil", cidade= "Curitiba", tara = 20000, capacidadeCarga = 15000, altura = 165,
+            largura = 154, comprimento = 300, motorista = "Tennorio Gomes", proprietario = "Tatiane Vasconcellos" },
+            new Veiculo() { codigo = 4, placa = "BXT-2154", frota = "Ford", propriedade = true , tipoVeiculo = "Automóvel", tipoCarroceria = "Hatch",
+                fabricante = "Fiat", modelo = "Uno", anoFabricacao = 2016, anoModelo = 2017, pais = "Brasil", cidade= "Curitiba", tara = 8000, capacidadeCarga = 5000, altura = 170,
+            largura = 154, comprimento = 300, motorista = "Ana Maltez", proprietario = "Marcelo Roberto" }
             };
             return listaVeiculos;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("BuscarVeiculo/{codigo}")]
+        public Veiculo BuscarVeiculo(int codigo) {
+            Veiculo veiculo = listaVeiculos.Where(v => v.codigo == codigo)
+                .Select(v => v)
+                .First();
+            return veiculo;
         }
 
     }
--- WebApi/Models/Veiculo.cs
+++ WebApi/Models/Veiculo.cs
@@ -5,69 +5,70 @@ using System.Web;
 
 namespace WebApi.Models {
     public class Veiculo {
-        private int codigo;
-        private string placa;
-        private string frota;
-        private Boolean propriedade;
-        private string tipoVeiculo;
-        private string tipoCarroceria;
-        private string fabricante;
-        private string modelo;
-        private int anoFabricacao;
-        private int anoModelo;
-        private string pais;
-        private string cidade;
-        private Decimal tara;
-        private Decimal capacidadeDeCarga;
-        private Decimal altura;
-        private Decimal largura;
-        private Decimal comprimento;
-        private string motorista;
-        private string proprietario;
+        private int _codigo;
+        private string _placa;
+        private string _frota;
+        private Boolean _propriedade;
+        private string _tipoVeiculo;
+        private string _tipoCarroceria;
+        private string _fabricante;
+        private string _modelo;
+        private int _anoFabricacao;
+        private int _anoModelo;
+        private string _pais;
+        private string _cidade;
+        private Decimal _tara;
+        private Decimal _capacidadeCarga;
+        private Decimal _altura;
+        private Decimal _largura;
+        private Decimal _comprimento;
+        private string _motorista;
+        private string _proprietario;
 
         public Veiculo() { }
-        public Veiculo(int codigo, string placa, string frota, Boolean propriedade, string tipoVeiculo, string tipoCarroceria, string fabricante, string modelo, int anoFabricacao, int anoModelo,
-            string pais, string cidade, Decimal tara, Decimal capacidadeDeCarga, Decimal altura, Decimal largura, Decimal comprimento, string motorista, string proprietario) {
-            this.Codigo = codigo;
-            this.Placa = placa;
-            this.Frota = frota;
-            this.Propriedade = propriedade;
-            this.TipoVeiculo = tipoVeiculo;
-            this.TipoCarroceria = tipoCarroceria;
-            this.Fabricante = fabricante;
-            this.Modelo = modelo;
-            this.AnoFabricacao = anoFabricacao;
-            this.AnoModelo = anoModelo;
-            this.Pais = pais;
-            this.Cidade = cidade;
-            this.Tara = tara;
-            this.CapacidadeDeCarga = capacidadeDeCarga;
-            this.Altura = altura;
-            this.Largura = largura;
-            this.Comprimento = comprimento;
-            this.Motorista = motorista;
-            this.Proprietario = proprietario;
+
+        public Veiculo(int codigo, string placa, string frota, bool propriedade, string tipoVeiculo, string tipoCarroceria, string fabricante, string modelo, int anoFabricacao, int anoModelo, string pais, string cidade, decimal tara, decimal capacidadeCarga, decimal altura, decimal largura, decimal comprimento, string motorista, string proprietario) {
+            _codigo = codigo;
+            _placa = placa;
+            _frota = frota;
+            _propriedade = propriedade;
+            _tipoVeiculo = tipoVeiculo;
+            _tipoCarroceria = tipoCarroceria;
+            _fabricante = fabricante;
+            _modelo = modelo;
+            _anoFabricacao = anoFabricacao;
+            _anoModelo = anoModelo;
+            _pais = pais;
+            _cidade = cidade;
+            _tara = tara;
+            _capacidadeCarga = capacidadeCarga;
+            _altura = altura;
+            _largura = largura;
+            _comprimento = comprimento;
+            _motorista = motorista;
+            _proprietario = proprietario;
         }
 
-        public int Codigo { get => codigo; set => codigo = value; }
-        public string Placa { get => placa; set => placa = value; }
-        public string Frota { get => frota; set => frota = value; }
-        public bool Propriedade { get => propriedade; set => propriedade = value; }
-        public string TipoVeiculo { get => tipoVeiculo; set => tipoVeiculo = value; }
-        public string TipoCarroceria { get => tipoCarroceria; set => tipoCarroceria = value; }
-        public string Fabricante { get => fabricante; set => fabricante = value; }
-        public string Modelo { get => modelo; set => modelo = value; }
-        public int AnoFabricacao { get => anoFabricacao; set => anoFabricacao = value; }
-        public int AnoModelo { get => anoModelo; set => anoModelo = value; }
-        public string Pais { get => pais; set => pais = value; }
-        public string Cidade { get => cidade; set => cidade = value; }
-        public decimal Tara { get => tara; set => tara = value; }
-        public decimal CapacidadeDeCarga { get => capacidadeDeCarga; set => capacidadeDeCarga = value; }
-        public decimal Altura { get => altura; set => altura = value; }
-        public decimal Largura { get => largura; set => largura = value; }
-        public decimal Comprimento { get => comprimento; set => comprimento = value; }
-        public string Motorista { get => motorista; set => motorista = value; }
-        public string Proprietario { get => proprietario; set => proprietario = value; }
+        public int codigo { get => _codigo; set => _codigo = value; }
+        public string placa { get => _placa; set => _placa = value; }
+        public string frota { get => _frota; set => _frota = value; }
+        public bool propriedade { get => _propriedade; set => _propriedade = value; }
+        public string tipoVeiculo { get => _tipoVeiculo; set => _tipoVeiculo = value; }
+        public string tipoCarroceria { get => _tipoCarroceria; set => _tipoCarroceria = value; }
+        public string fabricante { get => _fabricante; set => _fabricante = value; }
+        public string modelo { get => _modelo; set => _modelo = value; }
+        public int anoFabricacao { get => _anoFabricacao; set => _anoFabricacao = value; }
+        public int anoModelo { get => _anoModelo; set => _anoModelo = value; }
+        public string pais { get => _pais; set => _pais = value; }
+        public string cidade { get => _cidade; set => _cidade = value; }
+        public decimal tara { get => _tara; set => _tara = value; }
+       
+        public decimal altura { get => _altura; set => _altura = value; }
+        public decimal largura { get => _largura; set => _largura = value; }
+        public decimal comprimento { get => _comprimento; set => _comprimento = value; }
+        public string motorista { get => _motorista; set => _motorista = value; }
+        public string proprietario { get => _proprietario; set => _proprietario = value; }
+        public decimal capacidadeCarga { get => _capacidadeCarga; set => _capacidadeCarga = value; }
     }
 
 }
